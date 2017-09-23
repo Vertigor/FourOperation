54a6065f439ba428e98c990d9ee17ead096c2b9e
CHY\dell <353663785@qq.com>
2017-09-23 16:38:01 +0800

    Creat Class for Question, Frations and Calculate.

---
+++ src/question/Question.java
@@ -0,0 +1,16 @@
+package question;
+
+import java.util.Random;
+import java.util.Scanner;
+import fraction.Fraction.Fractions;
+import fraction.caculate.Caculate;
+
+public class Question {
+	private String[] operators;
+	private int[] operands;
+	private int operators_num;
+	public Question(){
+		
+	}
+
+}
